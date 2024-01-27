4388318c4a6d72e028ed16cc296ce36acbee6f94
Xun Wu (FA Talent) <v-wuxun@microsoft.com>
2024-01-28 00:35:00 +0800

    first commit

---
+++ deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.5.0'
+git_hash='unknown'
+git_branch='unknown'
+installed_ops={'cpu_adam': False, 'fused_adam': False, 'fused_lamb': False, 'sparse_attn': False, 'transformer': False, 'stochastic_transformer': False, 'async_io': False, 'transformer_inference': False, 'utils': False, 'quantizer': False}
+compatible_ops={'cpu_adam': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': True, 'transformer': True, 'stochastic_transformer': True, 'async_io': False, 'transformer_inference': True, 'utils': True, 'quantizer': True}
+torch_info={'version': '1.9', 'cuda_version': '10.2'}
